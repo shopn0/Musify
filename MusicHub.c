5422f26a69c42583f0b8a5f3880c41b7e94bfc2d
Shopno <shariare.shwapno@gmail.com>
2023-11-03 13:19:27 +0600

    2/6 functions workable

--- MusicHub.c
+++ MusicHub.c
@@ -18,23 +18,69 @@ void insertSong(char title[], char artist[], int duration){
     strcpy(newSong->title, title);
     strcpy(newSong->artist, artist);
     newSong->duration = duration;
-    newSong->next = newSong;
+    newSong->next = playlist;
+    playlist = newSong;
     printf("Song added to the playlist successfully.\n");
+    
 }
 
-void deleteSong(){}
-void searchSong(){}
+void deleteSong(char title[]){
+    struct Song* current = playlist;
+    struct Song* prev = NULL;
+
+       while (current!=NULL)
+       {
+        if (strcmp(current->title, title) == 0)
+        {
+            if (prev != NULL)
+            {
+                prev->next = current->next;
+            } else
+            {
+                playlist = current->next;
+            }
+            free (current);   
+            printf("Song deleted from the playlist.\n");
+            return;
+        }
+        prev = current;
+        current = current->next;        
+       }
+    printf("Song not found in the playlist.\n");        
+
+}
+void searchSong(char title[]){
+    struct Song* current = playlist;
+    while (current != NULL)
+    {
+        if (strcmp(current->title, title) == 0)
+        {
+            printf("\nSong found:\n");
+            printf("Title: %s\n", current->title);
+            printf("Artist: %s\n", current->artist);
+            printf("Duration: %d seconds\n", current->duration);
+            return;
+        }
+        current = current->next;
+    }
+    printf("Song not found in the playlist\n");
+}
 void disPlaylist(){}
-void about(){}
+void about(){
+    printf("1\n");
+    return;
+}
 int main()
 {
 
     int choice;
-    char title[50];
-    char artist[70];
+    char title[100];
+    char artist[100];
     int duration;
 
-    int consoleWidth = 80;  
+    while(1)
+    {
+    int consoleWidth = 40;  
     char text[] = "Musify"; 
     int len = strlen(text);
     int padding = (consoleWidth - len) / 2;
@@ -60,9 +106,9 @@ int main()
         {
         case 1:
             printf("\nEnter song details:\n");
-            printf("Titile\n");
+            printf("Titile: \n");
             scanf("%s", &title);
-            printf("Artist\n");
+            printf("Artist:\n");
             scanf("%s", &artist);
             printf("Duration (in seconds): \n");
             scanf("%d", &duration);
@@ -71,7 +117,7 @@ int main()
 
             break;
         case 2:
-            printf("Remove a song from the playlist\n");
+            printf("Enter song name to delete:\n");
             scanf("%s",title);
             deleteSong(title);
             
@@ -96,5 +142,8 @@ int main()
                     
         default:
             printf("Invalid Choice. Please try again.\n");
+           
         }
+    }
+    return 0;
 }
